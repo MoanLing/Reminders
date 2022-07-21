dab83bf365f131207fd60729f7322cff20a6a459
Moan Ling <mewmewmewcj@gmail.com>
2022-07-21 13:23:31 +0300

    configurated scroll-block with bugs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-const bgModal = document.querySelector('.modal-overlay'),
+// Modal JS
+
+const body = document.body,
+    bgModal = document.querySelector('.modal-overlay'),
     mainModal = document.querySelector('.modal-wrapper'),
     openModalBtn = document.querySelector('.add-new-btn'),
+    mainDocument = document.documentElement,
+    widthWithScroll = mainDocument.clientWidth,
+    widthWithoutScroll = body.offsetWidth,
     closeModalBtn = document.querySelector('.cancel-btn');
+console.log(widthWithScroll);
+console.log(widthWithoutScroll);
 
 function openModal() {
+    body.classList.add('clear-body-scroll');
+    body.style.marginRight = 
+    `${widthCalced}px`;
+
     bgModal.classList.add('modal-overlay-created');
     mainModal.classList.add('modal-wrapper-created');
+    
 
     setTimeout(() => {
         bgModal.classList.add('modal-overlay-activated')
@@ -17,11 +30,14 @@ function openModal() {
 }
 
 function closeModal() {
+    body.classList.remove('clear-body-scroll');
+    body.style.marginRight = "";
+
     bgModal.classList.remove('modal-overlay-activated');
     mainModal.classList.remove('modal-wrapper-activated');
 
     setTimeout(() => { bgModal.classList.remove('modal-overlay-created');
-    mainModal.classList.remove('modal-wrapper-created') }, 100)
+    mainModal.classList.remove('modal-wrapper-created') }, 500)
 }
 
 window.onclick = function (e) {
@@ -37,3 +53,10 @@ openModalBtn.addEventListener("click", function(){
 closeModalBtn.addEventListener("click", function(){
     closeModal()
 });
+
+
+// Scroll calc JS
+
+const widthCalced = widthWithScroll - widthWithoutScroll;
+console.log(`${widthCalced}px`);
+console.log(widthWithoutScroll - widthWithScroll); 
